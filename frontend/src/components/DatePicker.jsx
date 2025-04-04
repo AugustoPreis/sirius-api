5dd58ff4ad2be74f24b794587be75e0faa2ff041
Augusto Tomasi <augustopreisthomasi@gmail.com>
2025-04-04 11:24:06 -0300

    adiciona date picker personalizado

---
+++ frontend/src/components/DatePicker.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import dateFnsGenerateConfig from 'rc-picker/lib/generate/dateFns';
+import generatePicker from 'antd/es/date-picker/generatePicker';
+import ptBR from 'antd/es/date-picker/locale/pt_BR';
+import 'antd/es/date-picker/style/index';
+
+const Picker = generatePicker(dateFnsGenerateConfig);
+
+export default function DatePicker(props) {
+  const pickerProps = {
+    locale: ptBR,
+    format: 'dd/MM/yyyy',
+    style: { width: '100%' },
+    ...props,
+  };
+
+  //Converte datas em formato de string
+  if (typeof props.value === 'string') {
+    pickerProps.value = new Date(props.value);
+  }
+
+  return (
+    <Picker {...pickerProps} />
+  );
+}
