c3406d61139b42aaf942fb3586e187c88860bf64
Augusto Tomasi <augustopreisthomasi@gmail.com>
2025-04-08 14:48:14 -0300

    adiciona middleware de autenticação nas rotas de atendimento

--- backend/src/routes/atendimentoRoutes.js
+++ backend/src/routes/atendimentoRoutes.js
@@ -1,21 +1,22 @@
 const { Router } = require('express');
+const { authHandler } = require('../middlewares/authHandler');
 const { atendimentoController } = require('../controllers/atendimentoController');
 
 const routes = Router();
 
-routes.get('/', (req, res, next) => {
+routes.get('/', authHandler, (req, res, next) => {
   atendimentoController.listar(req, res, next);
 });
 
-routes.get('/ranking', (req, res, next) => {
+routes.get('/ranking', authHandler, (req, res, next) => {
   atendimentoController.ranking(req, res, next);
 });
 
-routes.get('/quantidade-por-funcionario', (req, res, next) => {
+routes.get('/quantidade-por-funcionario', authHandler, (req, res, next) => {
   atendimentoController.qtdAtendimentos(req, res, next);
 });
 
-routes.post('/', (req, res, next) => {
+routes.post('/', authHandler, (req, res, next) => {
   atendimentoController.cadastrar(req, res, next);
 });
 
