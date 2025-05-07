b9d20af13c0a40e88d4222154ab90caa05a4f62a
Augusto Tomasi <augustopreisthomasi@gmail.com>
2025-05-07 14:47:56 -0300

    adiciona gráfico de entradas x vendas com dados fixos

--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,9 +1,29 @@
+import { Card, Col, Divider, Row } from 'antd';
 import React from 'react';
+import DatePicker from '../../components/DatePicker';
+import GraficoEntradas from './graficos/Entradas';
 
 export default function Dashboard() {
 
   return (
-    <div>
-    </div>
+    <Card title='Dashboard'>
+      <Row gutter={[10, 10]}
+        style={{ marginBottom: 20 }}>
+        <Col xl={3}
+          lg={4}
+          md={6}
+          sm={8}
+          xs={24}>
+          Selecionar dia:
+          <DatePicker />
+        </Col>
+      </Row>
+      <Divider />
+      <Row gutter={[10, 20]}>
+        <Col span={24}>
+          <GraficoEntradas />
+        </Col>
+      </Row>
+    </Card>
   );
 }
---
+++ frontend/src/pages/dashboard/graficos/Entradas.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import ReactECharts from 'echarts-for-react';
+
+export default function Entradas() {
+  const option = {
+    title: { left: 'center', text: 'Entradas X Vendas no dia' },
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'shadow',
+      },
+    },
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      containLabel: true,
+    },
+    xAxis: {
+      type: 'category',
+      data: [
+        '00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00',
+        '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00',
+        '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00',
+      ],
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        name: 'Entradas',
+        type: 'line',
+        smooth: true,
+        data: [0, 0, 0, 0, 0, 0, 0, 0, 5, 15, 40, 80, 20, 40, 50, 80, 20, 40, 50, 80, 20, 5, 0, 0],
+      },
+      {
+        name: 'Vendas',
+        type: 'line',
+        smooth: true,
+        data: [0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 6, 1, 2, 2, 5, 1, 2, 3, 6, 1, 0, 0, 0],
+      },
+    ],
+  }
+
+  return (
+    <ReactECharts notMerge
+      option={option}
+      lazyUpdate={true}
+      style={{ minHeight: 400 }}
+      loadingOption={{
+        text: 'Carregando...',
+        color: '#1A8FbF',
+        fontSize: 24,
+      }} />
+  );
+}
